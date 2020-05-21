e1ea1e82678336b4547475aba205da4003f85ebd
mickel0134 <mickel0134@gmail.com>
2020-05-21 18:07:46 +0800

    Add project files.

---
+++ DSPBL/SplashForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DSPBL
+{
+    public partial class SplashForm : Form
+    {
+        Timer time = new Timer();
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+        private void SplashForm_Load(object sender, EventArgs e)
+        {
+            time.Tick += new EventHandler(Pbar);
+            time.Enabled = true;
+        }
+        void Pbar(object sender, EventArgs e)
+        {
+            progressBar1.Increment(5);
+            if (progressBar1.Value == 100) timer2.Enabled = true;
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ProgressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
